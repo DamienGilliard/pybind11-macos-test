827168b07e61c0eb8377c3e62ff0fd6ad49037d5
DamienGilliard <damien.gilliard@epfl.ch>
2025-04-13 14:48:39 +0200

    fix: PublicMethod now returns double the integer that is is being given

--- src/cpp.cc
+++ src/cpp.cc
@@ -2,11 +2,12 @@
 
 namespace dummyNamespace
 {
-    void SmallClass::publicMethod(int integer)
+    int SmallClass::PublicMethod(int integer)
     {
         std::cout << "This is a public method with integer: " << integer << std::endl;
+        return 2*integer;
     }
-    void SmallClass::privateMethod(int integer)
+    void SmallClass::PrivateMethod(int integer)
     {
         std::cout << "This is a private method with integer: " << integer << std::endl;
     }
